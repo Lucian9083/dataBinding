24b5766912ddfa2e80b15c7a1ab9e5a4a274893c
Lucian Lv <lucian9083@gmail.com>
2016-10-21 17:54:49 +0800

    add function

--- app/src/main/java/com/example/lucian/databinding/MainActivityFragment.java
+++ app/src/main/java/com/example/lucian/databinding/MainActivityFragment.java
@@ -1,10 +1,14 @@
 package com.example.lucian.databinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.lucian.databinding.databinding.FragmentMainBinding;
+import com.example.lucian.databinding.model.User;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,10 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        FragmentMainBinding fragmentMainBinding = DataBindingUtil.bind(view);
+        User user = new User();
+        fragmentMainBinding.setUser(user);
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/lucian/databinding/model/User.java
@@ -0,0 +1,13 @@
+package com.example.lucian.databinding.model;
+
+/**
+ * Created by lvzheng on 16/10/21.
+ */
+
+public class User {
+
+    public String userName = "Lucian";
+
+    public String passWord = "123456";
+
+}
